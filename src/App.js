ab3f574b6495347f2574f3b156a865cdda153332
Lucas <lucasmoc47@gmail.com>
2020-02-22 18:42:19 -0300

    Setting up components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExpenses />
+				<TransactionList />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Header() {
+	return (
+		<h2>Expense Tracker</h2>
+	);
+}
